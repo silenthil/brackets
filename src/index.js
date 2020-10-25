19a33fb19206b99d3a0b28ea2f7648ff9f14f349
Evgeny <s-a-w-13@mail.ru>
2020-10-25 06:38:52 +0300

    init:start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let delim=bracketsConfig.map((item) => {
+    if(Array.isArray(item)) return item.join('');
+  })
+  str=str.split('')
+  function f(x){
+    for (let i=0;i<str.length;i++){
+        if(str[i]+str[i+1]==x){
+           str.splice(i,2)
+           i=-1
+           return g();
+        }
+      }
+    return 
+  }
+  function g(){
+    for(let t=0;t<delim.length;t++){
+        f(delim[t])
+    }
+  }
+  g() 
+  if(str.length==0) return true 
+  else return false
 }
